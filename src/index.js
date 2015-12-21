c641d69079ea3eaeb0a098ac2532da78cf602803
wenlong.wwl <wenlong.wwl@alibaba-inc.com>
2015-12-21 23:46:24 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ export default function({ types }) {
             if (types.isImportSpecifier(spec)) {
               methodName = value + '/' + spec.imported.name;
             }
-          }); 
+            if (types.isImportDefaultSpecifier(spec)) {
+              methodName = value + '/' + spec.local.name;
+            }
+          });
+          methodName = methodName || 'noname';
         }
       },
 
@@ -23,15 +27,35 @@ export default function({ types }) {
         let t = types;
 
         if (!types.isIdentifier(node.callee)) {
-          if(object && object.name.toLowerCase()==='reactdom'){
-            let fn = 'function(render, name, argus){ render.apply(this, argus); }';
+          if(object && (object.name||'').toLowerCase()==='reactdom'){
+            let fn = `function(){
+              var argus = Array.prototype.slice.call(arguments),
+                rcDom = argus[1], el = argus[4], vdom = argus[5]; 
+              rcDom.render(vdom, el);
+            }`;
             let argus = t.arrayExpression(node.arguments);
             let icatReact = "window['ICAT']? window['ICAT'].react : ";
-            
+            let arr = [t.stringLiteral(methodName), node.callee.object];
+            node.arguments.map(arg => {
+              if(t.isJSXElement(arg)) {
+                arr.push(arg.openingElement.name);
+                if(arg.openingElement.attributes.length){
+                  let attrs = [];
+                  arg.openingElement.attributes.map(item=>{
+                    attrs.push(t.objectProperty(t.stringLiteral(item.name.name), item.value));
+                  });
+                  arr.push(t.objectExpression(attrs));
+                } else {
+                  arr.push(t.identifier('null'));
+                }
+              } else {
+                arr.push(arg);
+              }
+            });
+
+            arr.push(node.arguments[0]);
             path.replaceWith(
-              t.callExpression(
-                t.identifier("(" + icatReact + fn + ")"),
-                  [node.callee, t.stringLiteral(methodName), argus])
+              t.callExpression(t.identifier("(" + icatReact + fn + ")"), arr)
             )
           }
         }
