aad3dbe02dfb959f28cf374ba189b41fecb6a295
wenlong.wwl <wenlong.wwl@alibaba-inc.com>
2015-12-22 10:14:27 +0800

    add ReactForiCat

--- src/index.js
+++ src/index.js
@@ -18,6 +18,25 @@ export default function({ types }) {
           });
           methodName = methodName || 'noname';
         }
+
+        if(value==='react'){
+          let t = types;
+          let name;
+          node.specifiers.forEach(spec => {
+            if (types.isImportSpecifier(spec)) {
+              name = spec.imported.name;
+            }
+            if (types.isImportDefaultSpecifier(spec)) {
+              name = spec.local.name;
+            }
+          });
+          let assignEx = t.assignmentExpression(
+            '=', 
+            t.identifier('window.ReactForiCat'), 
+            t.identifier(name? name : 'null')
+          );
+          path.insertAfter(t.expressionStatement(assignEx));
+        }
       },
 
       CallExpression(path, state) {
@@ -35,11 +54,11 @@ export default function({ types }) {
             }`;
             let argus = t.arrayExpression(node.arguments);
             let icatReact = "window['ICAT']? window['ICAT'].react : ";
-            let arr = [t.stringLiteral(methodName), node.callee.object];
+            let arr = [t.stringLiteral(methodName), node.callee.object]; //[0]name, [1]rcDOM
             node.arguments.map(arg => {
               if(t.isJSXElement(arg)) {
-                arr.push(arg.openingElement.name);
-                if(arg.openingElement.attributes.length){
+                arr.push(arg.openingElement.name); //[2]widget
+                if(arg.openingElement.attributes.length){ //[3]argus
                   let attrs = [];
                   arg.openingElement.attributes.map(item=>{
                     attrs.push(t.objectProperty(t.stringLiteral(item.name.name), item.value));
@@ -49,11 +68,11 @@ export default function({ types }) {
                   arr.push(t.identifier('null'));
                 }
               } else {
-                arr.push(arg);
+                arr.push(arg); //[4]node
               }
             });
 
-            arr.push(node.arguments[0]);
+            arr.push(node.arguments[0]); //[5]vDom
             path.replaceWith(
               t.callExpression(t.identifier("(" + icatReact + fn + ")"), arr)
             )
